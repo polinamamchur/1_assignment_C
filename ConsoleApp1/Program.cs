a0142157163965b51ab7b67d0c43d13ee5f0d5b3
Polina <polinamam04@gmail.com>
2023-02-20 12:55:35 +0200

    class Queue.cs pull

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,9 @@
-﻿const string variable = "21 + (32 - 567)";
+﻿using ConsoleApp1;
+
+const string variable = "21 + (32 - 567)";
 ArrayList tokens = new ArrayList();
-Stack simbols = new Stack();
-//Queue<string> d = new Queue<string>();
+Stack opers = new Stack();
+Queue simbols = new Queue();
 char[] operators = new[] { '+', '-', '*', '/' };
 char[] brackets = new[] { '(', ')' };
 string buff = "";
@@ -22,7 +24,7 @@ foreach (var s in variable)
     }
     else
     {
-        if (buff != "")
+        if (buff.Length > 0)
         {
             tokens.Add(buff);
             buff = "";
@@ -33,7 +35,7 @@ foreach (var s in variable)
     }
 }
 
-if (buff != "")
+if (buff.Length > 0)
 { 
     tokens.Add(buff);
 }
@@ -45,9 +47,12 @@ if (buff != "")
 
 foreach (var el in tokens.GetElements())
 {
-    Console.WriteLine(el);
+    //Console.WriteLine(el);
+    int number;
+    bool isNumeric = int.TryParse(el, out number);
+    Console.WriteLine("el = " + el);
+    Console.WriteLine("isNumeric = " + isNumeric);
 }
-
 
 
 //foreach (var token in tokens)
@@ -55,115 +60,20 @@ foreach (var el in tokens.GetElements())
 
 //Stack<string> simbols = new Stack<string>(tokens);
 //foreach (var simbol in simbols) Console.WriteLine(simbol);
-//simbols.Pop("21");
+//simbols.Pop("21")
 
 
-public class ArrayList
-{
-    private string[] _array = new string[10];
+Queue b = new Queue();
+b.Push("3");
+b.Push("a");
+string pull = b.Pull();
+string pull2 = b.Pull();
+Console.WriteLine("pull = " + pull );
+Console.WriteLine("pull2 = " + pull2 );
 
-    private int _pointer = 0;
 
-    public void Add(string element)
-    {
-        _array[_pointer] = element;
-        _pointer += 1;
 
-        if (_pointer == _array.Length)
-        {
-            var extendedArray = new string[_array.Length * 2];
-            for (var i = 0; i < _array.Length; i++)
-            {
-                extendedArray[i] = _array[i];
-            }
 
-            _array = extendedArray;
-            //this also can be achieved via
-            //Array.Resize(ref _array, _array.Length * 2);
-        }
-    }
 
-    public void Remove(string element)
-    {
-        for (var i = 0; i < _pointer; i++)
-        {
-            if (_array[i] == element)
-            {
-                for (var j = i; j < _pointer - 1; j++)
-                {
-                    _array[j] = _array[j + 1];
-                }
 
-                _pointer -= 1;
-                return;
-            }
-        }
-    }
-
-    public string GetAt(int index)
-    {
-        return _array[index];
-    }
-
-    public int IndexOf(string element)
-    {
-        for (var i = 0; i < _array.Length; i++)
-        {
-            if (_array[i] == element)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
-    public bool Contains(string element)
-    {
-        return IndexOf(element) != -1;
-    }
-    
-    public int Count()
-    {
-        return _pointer;
-    }
-
-    public string[] GetElements()
-    {
-        return _array;
-    }
-}
-
-public class Stack
-{
-    private const int Capacity = 50;
-
-    private string[] _array = new string[Capacity];
-
-    private int _pointer;
-
-    public void Push(string value)
-    {
-        if (_pointer == _array.Length)
-        {
-            // this code is raising an exception about reaching stack limit
-            throw new Exception("Stack overflowed");
-        }
-
-        _array[_pointer] = value;
-        _pointer++;
-    }
-
-    public string Pull()
-    {
-        if (_pointer == 0)
-        {
-            //you can also raise an exception here, but we're simple returning nothing
-            return null;
-        }
-
-        var value = _array[_pointer];
-        _pointer--;
-        return value;
-    }
-}
 
